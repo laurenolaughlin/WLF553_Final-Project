0f1a826710fa3694f1c29aa2f4a9f752bfa6b115
laurenolaughlin <lolaughlin@uidaho.edu>
2024-12-05 10:07:26 -0800

    updated data sources

--- README.txt
+++ README.txt
@@ -1,5 +1,28 @@
 This is a repository created by Lauren O'Laughlin for WLF553's final project.
 
-The original data source proved to be inaccurate. Shifted to a new data source (USDA Census of Agriculture) with different available variables and connections. Edits to original Tidy Data Verse (see V02) and table design updated.
+11/21/24 log:
 
-This README was last edited 11/25/2024.
+Database was rebuilt. Old data set proved to be inaccurate, rebuilt using new variables available in new dataset. See below for data source information.
+
+
+#### Data Sources ####
+
+Data on the farm size and ownership is sourced from USDA Census of Agriculture.
+
+Population data from 2002 for all counties is from the Population Division of the US Census Bureau. 2022 data was sourced on the US Census Burea using American Community Survey Demographic and Housing Estimates. https://data.census.gov/table/ACSDP1Y2022.DP05?t=Populations%20and%20People&g=050XX00US30031&y=2022
+
+
+#### Last Edit Data #### 
+
+This README was last edited 12/2/24
+
+#### Questions to ask of the data! ####
+1. How has the population changed in each county in the two time periods? Both total numbers and percent change. 
+2. How has the total number of farms changed in each area? Total number and percent change. 
+3. What proportion of farms were in each size category in each year? 
+4. How has the proportion of farms changed per county in each county? 
+5. How has the proportion of farms changed overall in the time period? 
+6. Is there a correlation between population change and farm change? 
+
+
+
--- code/build_database.R
+++ code/build_database.R
@@ -1,61 +1,89 @@
-#load libraries ####
+# 2.1 load libraries and packages ####
 library(DBI)
 library(RSQLite)
+library(dplyr)
+install.packages("tidyverse")
 
-#connect to the existing database ####
-con <- dbConnect(RSQLite::SQLite(), dbname = "Final-Project.db");
 
-# run SQL code to create tables ####
 
-dbExecute(con, "
-CREATE TABLE IF NOT EXISTS county (
-county_id varchar(20) NOT NULL PRIMARY KEY,
-county varchar(15),
-state char(2)
-);
-")
+#2.2 connect to the existing database ####
+finalproject_db <- dbConnect(RSQLite::SQLite(), dbname = "Final-Project.db");
 
-dbExecute (con, "
-CREATE TABLE population (
-population_id varchar(20) NOT NULL PRIMARY KEY,
-county_id varchar(20),
-population_size varchar(20),
-year varchar(4),
-FOREIGN KEY (county_id) REFERENCES county(county_id)
-);
-")
- 
-dbExecute(con, "
-CREATE TABLE ownership (
-ownership_id varchar(20) NOT NULL PRIMARY KEY,
-owners varchar(10),
-partial_owners varchar(10),
-tenants varchar(10),
-year varchar(4),
-county_id varchar(20),
-FOREIGN KEY (county_id) REFERENCES county(county_id)
-);
-")
+#verify that all the tables are loaded 
 
-dbExecute(con, "
-CREATE TABLE farm_size (
-  farmsize_id varchar(10) NOT NULL PRIMARY KEY,
-  county_id varchar(20),
-  under_100_acres varchar(5),
-  acres_101_to_500 varchar(5),
-  acres_500_to_1000 varchar(5),
-  over_1000_acres varchar(5),
-  year varchar(4),
-  FOREIGN KEY (county_id) REFERENCES county(county_id)
-);
-")
-          
-# Optional: Verify tables were created. ####
-tables <- dbListTables(con)
-print(tables);
+dbListTables(finalproject_db)
 
-#Confirm that data was loaded properly ####
+#### import the data ####
 
+#### 3.1 add the county table 
+
+county <- read.csv("data_processed/county.csv",
+                    stringsAsFactors = FALSE)
+
+#check that the names align 
+names(county)
+
+# enter the csv into the sql table
+
+dbWriteTable(finalproject_db, "county", county, append = TRUE)
+
+####3.2 add the farm size table ####
+
+farm_size <- read.csv("data_processed/farm_size.csv",
+                     stringsAsFactors = FALSE)
+
+#check that the names align 
+names(farm_size)
+
+# enter the csv into the sql table
+
+dbWriteTable(finalproject_db, "farm_size", farm_size, append = TRUE)
+
+#### 3.3 add the population table ####
+
+population <- read.csv("data_processed/population.csv",
+                      stringsAsFactors = FALSE)
+
+#check that the names align
+
+names(population)
+
+#enter the csv into the sql table 
+dbWriteTable(finalproject_db, "population", population, append = TRUE)
+
+#### 3.4 enter the ownership table 
+
+ownership <- read.csv("data_processed/ownership.csv",
+                       stringsAsFactors = FALSE)
+
+#check that the names align 
+names(ownership)
+
+#enter the csv into the sql table
+dbWriteTable(finalproject_db, "ownership", ownership, append = TRUE)
+
+#### 3.5 confirm that the data was loaded correctly ####
 library(DBI)
-con <- dbConnect(RSQLite::SQLite(), dbname = "Final-Project.db");
-dbGetQuery(con, "SELECT * FROM population;")
+finalproject_db <-dbConnect(SQLite::SQLite(),dbname = "Final-Project.db")
+
+##county table
+
+dbGetQuery(finalproject_db, "SELECT * FROM county")
+
+##population table
+dbGetQuery(finalproject_db, "SELECT * FROM population")
+
+##farm size table
+dbGetQuery(finalproject_db, "SELECT * FROM farm_size")
+
+##onwership table 
+dbGetQuery(finalproject_db, "SELECT * FROM ownership")
+
+#### 4 data manipulation ####
+
+#### Which area has the smallest population? ####
+
+population <-DBI::dbReadTable(finalproject_db, "population")
+
+
+
